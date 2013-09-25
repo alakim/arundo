445e446beca56f2822987d70b1821733ef0c87e6
alvakim@gmail.com <alvakim@gmail.com@b0536a9e-5333-b5a2-e793-464177cb8627>
2013-09-25 10:40:03 +0000

    Отдельные экземпляры RowEditor для каждого CatalogEditor.

--- trunk/js/catalogEditor.js
+++ trunk/js/catalogEditor.js
@@ -1,47 +1,47 @@
 ﻿(function($, $A, H){
-	$.fn.catalogEditor = (function(rootID){
-		function template(){with(H){
-			return div({"class":"easyui-layout", style:"width:1200px;height:400px;"},
-				div({"data-options":"region:'west',split:true", title:$A.locale.getItem("catalogs"), style:"width:220px;"},
-					ul({"class":"catTreePnl"})
-				),
-				div({"data-options":"region:'center',title:false"},
-					div({"class":"dataGridPnl"})
-				)
-			);
-		}}
+	function template(){with(H){
+		return div({"class":"easyui-layout", style:"width:1200px;height:400px;"},
+			div({"data-options":"region:'west',split:true", title:$A.locale.getItem("catalogs"), style:"width:220px;"},
+				ul({"class":"catTreePnl"})
+			),
+			div({"data-options":"region:'center',title:false"},
+				div({"class":"dataGridPnl"})
+			)
+		);
+	}}
+	
+	function buildEditor(pnl, rowEditor, rootID){
+		pnl.html(template())
+			.find(".easyui-layout").layout();
 		
-		function buildEditor(pnl){
-			pnl.html(template())
-				.find(".easyui-layout").layout();
-			
-			pnl.find(".catTreePnl").tree({
-				loader: function(prm, onSuccess, onError){
-					$A.dataSource.getCatalogTree({rootID: rootID}, onSuccess, onError);
-				},
-				onClick: function(node){
-					$A.dataSource.getColumns(node.id, function(columns){
-						pnl.find(".dataGridPnl").datagrid({
-							loader: $A.dataSource.getTable,
-							columns:[columns],
-							queryParams:{catID:node.id}
-						});
+		pnl.find(".catTreePnl").tree({
+			loader: function(prm, onSuccess, onError){
+				$A.dataSource.getCatalogTree({rootID: rootID}, onSuccess, onError);
+			},
+			onClick: function(node){
+				$A.dataSource.getColumns(node.id, function(columns){
+					pnl.find(".dataGridPnl").datagrid({
+						loader: $A.dataSource.getTable,
+						columns:[columns],
+						queryParams:{catID:node.id}
 					});
-				}
-			});
-			$A.dataSource.getColumns(rootID, function(columns){
-				pnl.find(".dataGridPnl").datagrid({
-					loader: $A.dataSource.getTable,
-					columns: [columns],
-					queryParams:{catID:rootID},
-					singleSelect: true,
-					onClickRow: RowEditor.open
-				})
+				});
+			}
+		});
+		$A.dataSource.getColumns(rootID, function(columns){
+			pnl.find(".dataGridPnl").datagrid({
+				loader: $A.dataSource.getTable,
+				columns: [columns],
+				queryParams:{catID:rootID},
+				singleSelect: true,
+				onClickRow: rowEditor.open
 			})
-		}
-		
+		})
+	}
+	
+	$.fn.catalogEditor = (function(rootID){
 		$(this).each(function(i,el){
-			buildEditor($(el));
+			buildEditor($(el), new RowEditor(), rootID);
 		});
 	});
 })(jQuery, Arundo, Html);
--- trunk/js/rowEditor.js
+++ trunk/js/rowEditor.js
@@ -1,5 +1,4 @@
 ﻿var RowEditor = (function($, $A, H){
-
 	var dlgID = "rowEditorDlg";
 	
 	var templates = {
@@ -31,61 +30,60 @@
 		});
 		return res;
 	}
-	
-	var __={
-		open:function(rowIdx, rowData){
-			if(!$("#"+dlgID).length){
-				$("body").append(Html.div({id:dlgID}));
-				$("#"+dlgID)
-					.dialog({
-						title:$A.locale.getItem("rowEditor"), 
-						width: 450, height: 300,
-						resizable: true,
-						buttons:[
-							{text:$A.locale.getItem("btOK"), handler:function(){
-								saveData(function(){
+
+	return function(){
+		
+		$.extend(this, {
+			open:function(rowIdx, rowData){
+				if(!$("#"+dlgID).length){
+					$("body").append(Html.div({id:dlgID}));
+					$("#"+dlgID)
+						.dialog({
+							title:$A.locale.getItem("rowEditor"), 
+							width: 450, height: 300,
+							resizable: true,
+							buttons:[
+								{text:$A.locale.getItem("btOK"), handler:function(){
+									saveData(function(){
+										$("#"+dlgID).dialog("close");
+									});
+								}},
+								{text:$A.locale.getItem("btCancel"), handler:function(){
 									$("#"+dlgID).dialog("close");
-								});
-							}},
-							{text:$A.locale.getItem("btCancel"), handler:function(){
-								$("#"+dlgID).dialog("close");
-							}}
-						]
-					});
+								}}
+							]
+						});
+				}
+				var contentPnl = $("#"+dlgID+" .dialog-content");
+				contentPnl.html("loading...");
+				
+				function saveData(onSuccess){
+					var data = collectData(contentPnl);
+					$A.dataSource.saveRecord(rowData.id, data, onSuccess, $A.displayError);
+				}
+				
+				$("#"+dlgID).dialog("open");
+				$A.dataSource.getRecord(rowData.id, function(data){
+					contentPnl.html(templates.dialog(data))
+						.find(".dateFld").datebox({
+							formatter: function(d){
+								var y = d.getFullYear();
+								var m = d.getMonth()+1;
+								var d = d.getDate();
+								if(m<10) m = "0"+m;
+								if(d<10) d = "0"+d;
+								return [d, m, y].join(".");
+							},
+							parser: function(s){
+								var date = s.split(".");
+								var d = parseInt(date[0], 10);
+								var m = parseInt(date[1], 10);
+								var y = parseInt(date[2], 10);
+								return new Date(y, m-1, d);
+							}
+						});
+				}, $A.displayError);
 			}
-			var contentPnl = $("#"+dlgID+" .dialog-content");
-			contentPnl.html("loading...");
-			
-			function saveData(onSuccess){
-				var data = collectData(contentPnl);
-				$A.dataSource.saveRecord(rowData.id, data, onSuccess, $A.displayError);
-			}
-			
-			$("#"+dlgID).dialog("open");
-			$A.dataSource.getRecord(rowData.id, function(data){
-				contentPnl.html(templates.dialog(data))
-					.find(".dateFld").datebox({
-						formatter: function(d){
-							var y = d.getFullYear();
-							var m = d.getMonth()+1;
-							var d = d.getDate();
-							if(m<10) m = "0"+m;
-							if(d<10) d = "0"+d;
-							return [d, m, y].join(".");
-						},
-						parser: function(s){
-							var date = s.split(".");
-							var d = parseInt(date[0], 10);
-							var m = parseInt(date[1], 10);
-							var y = parseInt(date[2], 10);
-							return new Date(y, m-1, d);
-						}
-					});
-			}, $A.displayError);
-		}
+		});
 	};
-	
-	
-	
-	return __;
 })(jQuery, Arundo, Html);
