bea9c763df96bb44e7694bbac12707398e6f7888
alvakim <alvakim@gmail.com>
2013-09-30 13:12:14 +0000

    Тесты источника данных

--- js/dataSources/iDataSource.js
+++ js/dataSources/iDataSource.js
@@ -6,21 +6,36 @@ var IDataSource = (function(){
 			"getRecord",
 			"saveRecord", 
 			"getTableColumns",
+			"getTable",
 			"getAllColumns", 
 			"deleteRows",
 			"deleteCatalog", 
-			"getCatalogProperties"
+			"getCatalogProperties",
+			"saveCatalogProperties"
 		],
 		"string":[
 			"name"
 		]
 	};
+	
+	var iTypes = {};
+	(function(){
+		for(var k in iDefinition){var defList = iDefinition[k];
+			for(var i=0; i<defList.length; i++){var itm = defList[i];
+				iTypes[itm] = k;
+			}
+		}
+	})();
 
 	function checkInterface(obj, type, members){
 		$.each(members, function(i, m){
 			if(typeof(obj[m])!=type)
 				__.displayConfigError("Missing Data Source interface member "+type+"::"+m);
-		})
+		});
+		for(var k in obj){
+			var t = iTypes[k];
+			if(!t) __.displayConfigError("Member "+type+"::"+k+" is not defined in interface IDataSource.");
+		}
 	}
 
 
