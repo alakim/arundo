7f7abcb7fce6e9891d255a701c45822cf0837255
alvakim <alvakim@gmail.com>
2013-10-08 11:57:06 +0000

    Вывод записей из XmlUsersDB

--- ws/providers/xmlusersdb.php
+++ ws/providers/xmlusersdb.php
@@ -30,6 +30,32 @@ class XmlUsersDB{
 		echo("{\"field\":\"name\",\"title\":\"Название\"}");
 		echo(']');
 	}
+	
+	function writeTableRows($tblRef, $dbCatID){
+		$db = $tblRef['usersDBID'];
+		$sectID = $tblRef['section'];
+		
+		if($db=='') return;
+		$doc = new DOMDocument('1.0', 'UTF-8');
+		$doc->load('xmlData/'.$db);
+		$xp = new DOMXPath($doc);
+		
+		$q = $sectID=='userGroups'?'//groups/group':(
+			$sectID=='userAccounts'?'//users/user':'');
+			
+		if($q=='') return;
+		$rows = $xp->query($q);
+		
+		echo('[');
+		$first = true;
+		foreach($rows as $row){
+			$rID = $row->getAttribute('id');
+			$rNm = TreeUtility::conv($row->getAttribute('name'));
+			if($first) $first = false; else echo(',');
+			echo("{\"id\":\"$rID\",\"name\":\"$rNm\"}");
+		}
+		echo(']');
+	}
 }
 
 
