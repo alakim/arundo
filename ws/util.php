eb28986c0ccb7dd7acd5914e4fc7a85b1303e1b0
alvakim <alvakim@gmail.com>
2013-10-11 12:01:32 +0000

    Проверка прав пользователей

--- ws/util.php
+++ ws/util.php
@@ -4,5 +4,18 @@ class Util{
 	static function conv($str){
 		return iconv("UTF-8", "windows-1251", $str);
 	}
+	
+	static function getUserPermissions($ticket){
+		$sessions = ProviderFactory::getSessions('xmlData/');
+		$userID = $sessions->getAuthorizedUser($ticket);
+
+		if($userID==null){
+			echo('{error:"errAuthorizationRequired"}');
+			die();
+		}
+
+		$userProvider = ProviderFactory::getUsers();
+		return $userProvider->getUserPermissions($userID);
+	}
 }
 
