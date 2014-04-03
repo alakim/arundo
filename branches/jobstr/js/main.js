b7fe87548f48c85ad48b8fcac54debe7a335f1e5
alvakim@gmail.com <alvakim@gmail.com@b0536a9e-5333-b5a2-e793-464177cb8627>
2014-04-03 08:11:47 +0000

    Базовая архитектура

---
+++ branches/jobstr/js/main.js
@@ -0,0 +1,28 @@
+﻿requirejs.config({
+    baseUrl: "js",
+    paths: {
+		jquery: "lib/jquery-1.7.2.min",
+		html:"lib/html",
+		knockout:"lib/knockout-3.1.0",
+        lib: "lib"
+    },
+	shim:{
+		"html":{exports:"Html"}
+	}
+});
+
+requirejs(["jquery", "html", "knockout"], function   ($, $H, $K) {
+	$(".mainPanel").html((function(){with($H){
+		return div({"class":"testPnl"},
+			"SUCCESS",
+			p("name: ", span({"data-bind":"text:name"}))
+		)
+	}})());
+	
+	
+	function TestModel(){
+		this.name = "test model";
+	}
+	
+	$K.applyBindings(new TestModel(), $(".testPnl")[0]);
+});
