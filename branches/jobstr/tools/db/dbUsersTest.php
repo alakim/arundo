2c60487015c9ac53dbe4f63ec643c5f8a7b3fe2d
alvakim@gmail.com <alvakim@gmail.com@b0536a9e-5333-b5a2-e793-464177cb8627>
2014-04-16 07:45:43 +0000

    Авторизация пользователя

--- branches/jobstr/tools/db/dbUsersTest.php
+++ branches/jobstr/tools/db/dbUsersTest.php
@@ -5,6 +5,7 @@ class DbUsersTest extends DbUsers{
 	
 	public function testFill($con){
 		$fields = "Name, Login, Password";
+		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Системный администратор\", \"sa\", \"sa\")");
 		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Администратор\", \"admin\", \"admin\")");
 		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Иванов И.И.\", \"ivanov\", \"ivanov\")");
 		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Петров П.П.\", \"petrov\", \"petrov\")");
