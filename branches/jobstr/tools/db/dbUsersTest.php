30619afc48264dae36e38eabf5acf45598dd9a4d
alvakim@gmail.com <alvakim@gmail.com@b0536a9e-5333-b5a2-e793-464177cb8627>
2014-04-16 08:09:15 +0000

    Авторизация на основе тикетов

--- branches/jobstr/tools/db/dbUsersTest.php
+++ branches/jobstr/tools/db/dbUsersTest.php
@@ -3,13 +3,17 @@ require_once('db/dbUsers.php');
 
 class DbUsersTest extends DbUsers{
 	
+	private function stdCrypt($str){
+		return crypt($str, cryptKey);
+	}
+	
 	public function testFill($con){
 		$fields = "Name, Login, Password";
-		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Системный администратор\", \"sa\", \"sa\")");
-		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Администратор\", \"admin\", \"admin\")");
-		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Иванов И.И.\", \"ivanov\", \"ivanov\")");
-		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Петров П.П.\", \"petrov\", \"petrov\")");
-		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Сидоров С.С.\", \"sidorov\", \"sidorov\")");
+		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Системный администратор\", \"sa\", \"".(DbUsersTest::stdCrypt("sa"))."\")");
+		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Администратор\", \"admin\", \"".(DbUsersTest::stdCrypt("admin"))."\")");
+		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Иванов И.И.\", \"ivanov\", \"".(DbUsersTest::stdCrypt("ivanov"))."\")");
+		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Петров П.П.\", \"petrov\", \"".(DbUsersTest::stdCrypt("petrov"))."\")");
+		execSql($con, "INSERT INTO ".$this->tableName." (".$fields.") VALUES (\"Сидоров С.С.\", \"sidorov\", \"".(DbUsersTest::stdCrypt("sidorov"))."\")");
 	
 	}
 	
